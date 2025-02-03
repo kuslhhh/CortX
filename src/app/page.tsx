e16b6f6f25f0074297d1aa8fc4a328ddd5063707
kuslhhh <kushaljadhav7745@gmail.com>
2025-02-03 15:18:07 +0530

    Button comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 "use client"
 
+import PlusIcon from "@/components/icons/plusIcon";
+import ShareIcon from "@/components/icons/shareIcon";
+import Button from "@/components/ui/button";
+import Card from "@/components/ui/card";
+
 export default function Home() {
 
   return (
-    
-    <div className="justify-center items-center h-screen flex">
 
+    <div className="flex items-center justify-center h-screen ">
+      {/* <Button varient="primary" text="Add Content" startIcon={<PlusIcon />} />
+      <Button varient="secondary" text="Share Brain" startIcon={<ShareIcon />}/> */}
+
+      <Card/>
     </div>
 
   );
---
+++ src/components/ui/card.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Card() {
+    return (
+
+        <div>
+            <div className='bg-[#1a1a1a] rounded-l-lg  border border-y-0 border-l-0 border-r-[#a9a9a9]'>
+                this is a card component
+            </div>
+        </div>
+    )
+}
