3e4e62f709d4819d956f1f2174df68333f72d50e
kuslhhh <kushaljadhav7745@gmail.com>
2025-02-04 12:46:53 +0530

    update layout and sidebar, add Instagram icon, and refactor page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,74 +1,11 @@
 "use client"
 
-import PlusIcon from "@/components/icons/plusIcon";
-import ShareIcon from "@/components/icons/shareIcon";
-import XIcon from "@/components/icons/XIcon";
-import Button from "@/components/ui/button";
-import Card from "@/components/ui/card";
-import Modal from "@/components/ui/modal";
-import Sidebar from "@/components/ui/sidebar";
-import { useState } from "react";
+import Dashboard from "@/pages/Dashboard";
 
 export default function Home() {
 
-  const [modalOpen, setModalOpen] = useState(false)
 
   return (
-    <>
-
-      <Sidebar />
-
-      <div className="p-4 ml-56 min-h-screen">
-
-        <Modal open={modalOpen} onClose={() => {
-          setModalOpen(false)
-        }} />
-
-        <div className="flex justify-end gap-2">
-
-          <Button onClick={() => {
-            setModalOpen(true)
-          }} startIcon={<PlusIcon />} text="Add Brain" varient="secondary" />
-          <Button startIcon={<ShareIcon />} text="Share Brain" varient="primary" />
-        </div>
-
-        <div className="container mx-auto p-4 ">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pl-4 gap-4">
-            <Card
-              title="Youtube"
-              type="youtube"
-              link="https://www.youtube.com/watch?v=rYDEJN3PMTM"
-            />
-
-            <Card
-              title="Spotify"
-              type="spotify"
-              link="https://open.spotify.com/track/3tgdOveYac7YMEAQD9sGLf?si=7a6225fd1b734a11"
-            />
-
-            <Card
-              title="Tweet"
-              type="twitter"
-              link="https://x.com/cryptobeastreal/status/1886477220387991838"
-            />
-            <Card
-              title="Reels"
-              type="instagram"
-              link="https://www.instagram.com/reel/DDcSMrUTYtw/?utm_source=ig_web_button_share_sheet&igsh=MzRlODBiNWFlZA=="
-            />
-            <Card
-              title="Reels"
-              type="instagram"
-              link="https://www.instagram.com/reel/DFYWTdVIvkc/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
-            />
-            <Card
-              title="Reels"
-              type="instagram"
-              link="https://www.instagram.com/reel/DDcSMrUTYtw/?utm_source=ig_web_button_share_sheet&igsh=MzRlODBiNWFlZA=="
-            />
-          </div>
-        </div>
-      </div>
-    </>
+    <Dashboard />
   );
 }
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,17 +1,25 @@
-import React, { ReactElement } from 'react'
 import SideBarItem from './SidebarItem'
 import XIcon from '../icons/XIcon'
 import YouTubeIcon from '../icons/YouTubeIcon'
 import SpotifyIcon from '../icons/SpotifyIcon'
+import LogoIcon from '../icons/logoIcon'
+import InstaIcon from '../icons/InstaIcon'
 
 export default function Sidebar() {
 
     return (
         <div className=' h-screen bg-[#1a1a1a] w-56 fixed rounded-r-[40] border border-y-0 border-l-0 border-r-[#a9a9a9]'>
-            <div className='pt-20 p-7'>
+
+            {/* <Image alt='Cortex' src="/cortex-logo.png" width={300} height={300} /> */}
+            <div className='-m-4 '>
+                <LogoIcon />
+            </div>
+
+            <div className='pt-5 p-7'>
                 <SideBarItem icon={<XIcon />} text='Tweets' />
                 <SideBarItem icon={<YouTubeIcon />} text='YouTube' />
                 <SideBarItem icon={<SpotifyIcon />} text='Spotify' />
+                <SideBarItem icon={<InstaIcon />} text='Instagram' />
             </div>
         </div>
     )
